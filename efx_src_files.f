77e9f997a3cd20bdf7abc3e7e0b07d57fcabf1a0
till straumann <till.straumann@alumni.tu-berlin.de>
2026-06-13 23:43:06 +0200

    added efx_src_files.f list

---
+++ efx_src_files.f
@@ -0,0 +1,8 @@
+# source file list - not including test benches
+# for use by efinity
+hdl/UartRx.vhd
+hdl/UartWrapper.vhd
+hdl/RateGenerator.vhd
+hdl/LongDivider.vhd
+hdl/UartTx.vhd
+hdl/UartPkg.vhd
